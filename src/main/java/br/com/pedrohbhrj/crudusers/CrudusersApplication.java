947edaaaab38463e152eb5df74e07f7f6b84fd6e
pedro <l2pedro157@gmail.com>
2026-05-23 15:50:12 -0300

    initial setup

---
+++ src/main/java/br/com/pedrohbhrj/crudusers/CrudusersApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrohbhrj.crudusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudusersApplication.class, args);
+	}
+
+}
